1d3918671e3c45614e6a6e5b5cc5911390038db9
irenelim <irenelim99@gmail.com>
2019-10-14 01:25:25 +0800

    try 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const routes = require('./route');
 
 
-console.log(routes.someText);
+console.log(routes.someText + "try");
 const server = http.createServer(routes.handler);
 
 server.listen(3000);
